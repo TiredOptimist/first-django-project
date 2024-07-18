339f10c8d3caed6c05a1be3af1d31b1fdc1887dd
TiredOptimist <pankovasveta210305@gmail.com>
2024-07-18 19:13:19 +0300

    Create new model

--- first/todo_list/admin.py
+++ first/todo_list/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from todo_list.models import ToDoItem
+
+
+@admin.register(ToDoItem)
+class ToDoItemAdmin(admin.ModelAdmin):
+    list_display = "id", "title", "done"
+    list_display_links = "id", "title"
--- first/todo_list/apps.py
+++ first/todo_list/apps.py
@@ -4,5 +4,6 @@ from django.apps import AppConfig
 class TodoListConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'todo_list'
+    verbose_name = "ToDo List"
 
 
