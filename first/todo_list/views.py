dc31aa5c1a59fb4d72f9b7df7d62aa6da0e972ce
TiredOptimist <pankovasveta210305@gmail.com>
2024-07-25 22:36:40 +0300

    Create hz

--- first/todo_list/views.py
+++ first/todo_list/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+from .models import ToDoItem
+
+
+def index_view(request: HttpRequest) -> HttpResponse:
+    todo_item = ToDoItem.objects.order_by("id").all()
+    # todo_item = ["Item1", "Item2",]
+    return render(request, template_name="todo_list/index.html", context={"todo_items": todo_item},)
