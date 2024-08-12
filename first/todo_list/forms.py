c3480730afe2e9f0e8bd18abb672d8cae45c8c76
HollyGooffy <nikita.byc@mail.ru>
2024-08-12 12:57:55 +0500

    "about and cards"

--- first/todo_list/forms.py
+++ first/todo_list/forms.py
@@ -6,6 +6,6 @@ from .models import ToDoItem
 class ToDoItemForm(forms.ModelForm):
     class Meta:
         model = ToDoItem
-        fields = ('title', 'description')
+        fields = ['title', 'description']
 
     description = forms.CharField(required=False)
