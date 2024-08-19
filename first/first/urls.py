9227fe81194b2ce0a1cdc4bf58bed086a285f54b
HollyGooffy <nikita.byc@mail.ru>
2024-08-20 00:17:44 +0500

    "about and cards"

--- first/first/urls.py
+++ first/first/urls.py
@@ -20,7 +20,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="index.html"), name="index"),
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
     path('todos/', include('todo_list.urls')),
     path("biography/", TemplateView.as_view(template_name="biography.html"), name="biography"),
     path('registration/', include('registration.urls')),
