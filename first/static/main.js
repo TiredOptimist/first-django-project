b99b613276b60afb4cddd0c1893cfcb62194d796
HollyGooffy <nikita.byc@mail.ru>
2024-08-27 19:11:52 +0500

    do new theme

--- first/static/main.js
+++ first/static/main.js
@@ -1,38 +1,28 @@
-let currentThemeName = localStorage.getItem('theme') || 'light';
-
-const themeButtons = document.querySelectorAll('.button-theme');
-
-themeButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const themeName = button.id;
-        switch(themeName) {
-            case 'dark':
-                applyTheme('dark');
-                break;
-            case 'light':
-                applyTheme('light');
-                break;
-            case 'pinky':
-                applyTheme('pinkystyle');
-                break;
-            case 'black':
-                applyTheme('dark'); // Используем темную тему для черной темы
-                break;
-        }
-    });
-});
-
-function applyTheme(theme) {
+function setTheme(theme) {
+    document.documentElement.classList.remove('dark','lightstyle', 'pinkystyle', 'total-black');
+    document.documentElement.classList.add(theme);
     localStorage.setItem('theme', theme);
-    
-    document.body.classList.remove('lightstyle', 'darkstyle', 'pinkystyle');
-    document.body.classList.add(theme);
-
-    const themeElement = document.querySelector(`#${theme}`);
-    if (themeElement) {
-        themeElement.style.backgroundColor = '#007bff';
-    }
 }
 
-// Инициализация темы при загрузке страницы
-applyTheme(currentThemeName);
+document.addEventListener('DOMContentLoaded', function() {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+        setTheme(savedTheme);
+    }
+
+    document.getElementById('dark').addEventListener('click', function() {
+        setTheme('dark');
+    });
+
+    document.getElementById('light').addEventListener('click', function() {
+        setTheme('lightstyle');
+    });
+
+    document.getElementById('pinky').addEventListener('click', function() {
+        setTheme('pinkystyle');
+    });
+
+    document.getElementById('black').addEventListener('click', function() {
+        setTheme('total-black');
+    });
+})
