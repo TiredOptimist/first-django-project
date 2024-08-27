5af3a9497b50fab627d4d38578252033c814b634
HollyGooffy <nikita.byc@mail.ru>
2024-08-27 15:11:04 +0500

    changes settings

--- first/static/main.js
+++ first/static/main.js
@@ -1,14 +1,38 @@
-document.addEventListener('DOMContentLoaded',
-    function () {
-        const navItems = document
-            .querySelectorAll('.dis-active');
- 
-        navItems.forEach(item => {
-            item.addEventListener('click',
-                function () {
-                    navItems.forEach(navItem => navItem
-                        .classList.remove('active'));
-                    this.classList.add('active');
-                });
-        });
+let currentThemeName = localStorage.getItem('theme') || 'light';
+
+const themeButtons = document.querySelectorAll('.button-theme');
+
+themeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const themeName = button.id;
+        switch(themeName) {
+            case 'dark':
+                applyTheme('dark');
+                break;
+            case 'light':
+                applyTheme('light');
+                break;
+            case 'pinky':
+                applyTheme('pinkystyle');
+                break;
+            case 'black':
+                applyTheme('dark'); // Используем темную тему для черной темы
+                break;
+        }
     });
+});
+
+function applyTheme(theme) {
+    localStorage.setItem('theme', theme);
+    
+    document.body.classList.remove('lightstyle', 'darkstyle', 'pinkystyle');
+    document.body.classList.add(theme);
+
+    const themeElement = document.querySelector(`#${theme}`);
+    if (themeElement) {
+        themeElement.style.backgroundColor = '#007bff';
+    }
+}
+
+// Инициализация темы при загрузке страницы
+applyTheme(currentThemeName);
